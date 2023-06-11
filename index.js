ff9aa9f3545ead41f73e2a7eed5dcdf25ca578c7
Pranav <epranavasai@gmail.com>
2023-06-11 10:36:12 +0530

    initial commit.

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(...args) {
+  return args.reduce(function (aggregate, num) {
+    return aggregate + num;
+  });
+}
+
+function mul(...args) {
+  return args.reduce(function (aggregate, num) {
+    return aggregate * num;
+  }, 1);
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  add,
+  mul,
+  div,
+  sub,
+};
